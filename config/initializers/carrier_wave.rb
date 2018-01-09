931cc739b344de965b1f51358d7d1f8ad51636b5
Erickson <ericksonsuarezocumarez@gmail.com>
2018-01-09 14:34:23 -0400

    Insert keys

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
 	CarrierWave.configure do |config|
 		config.fog_credentials = {
 			:provider => 'AWS',
-			:aws_access_key_id => ENV['S3_ACCESS_KEY'],
-			:aws_secret_access_key => ENV['S3_SECRET_KEY']
+			:aws_access_key_id => ENV['AKIAJPRRB7OYU75X2EBQ'],
+			:aws_secret_access_key => ENV['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']
 		} 
 
-		config.fog_directory = ENV['S3_BUCKET']
+		config.fog_directory = ENV['saffronimages']
 	end
 end
